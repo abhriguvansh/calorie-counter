ba262262d5623cc02100a0f34962ccaff489bc56
Aman <abhriguvansh99@gmail.com>
2020-05-30 20:35:48 -0700

    created ingredient field to be stored in database

---
+++ backend/models/ingredient.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ingredientSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 1,
+    },
+    calories: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    protein: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ingredient = mongoose.model('Ingredient', ingredientSchema);
+module.exports = User;
